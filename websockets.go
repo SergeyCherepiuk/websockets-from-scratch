e3076324fc68b4b828231ae481fe54552e1547c6
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-07-14 08:49:32 +0200

    Sending message part is completed

--- websockets.go
+++ websockets.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha1"
 	"encoding/base64"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -32,6 +34,9 @@ func (conn Connection) HandleCommunication(c net.Conn) {
 
 	wg.Add(1)
 	go receiveMessage(c)
+
+	wg.Add(1)
+	go sendingMessage(c)
 }
 
 func receiveMessage(c net.Conn) {
@@ -42,21 +47,18 @@ func receiveMessage(c net.Conn) {
 		// Read first two configuration bytes
 		_, err := io.ReadFull(c, configurationBytesBuffer)
 		if err != nil {
-			log.Println(err.Error())
 			continue
 		}
 
 		// Determine payload length and mask
 		payloadLength, mask, err := getPayloadLengthAndMask(configurationBytesBuffer, c)
 		if err != nil {
-			log.Println(err.Error())
 			continue
 		}
 
 		// Read payload
 		payload, err := readPayload(payloadLength, c)
 		if err != nil {
-			log.Println(err.Error())
 			continue
 		}
 
@@ -66,6 +68,36 @@ func receiveMessage(c net.Conn) {
 	}
 }
 
+func sendingMessage(c net.Conn) {
+	defer wg.Done()
+
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		// Read message from stdin
+		message, err := inputReader.ReadString('\n')
+		if err != nil {
+			continue
+		}
+
+		message = strings.TrimSpace(message)
+		if len(message) < 1 {
+			continue
+		}
+
+		// Determine payload length
+		payloadLength := getPayloadLength(message)
+
+		// Create frame and set first bytes (FIN + opcode)
+		frame := createFrame(message, payloadLength)
+
+		// Send frame to the client
+		if _, err := c.Write(frame); err != nil {
+			continue
+		}
+	}
+}
+
+// Receiving messages functions
 func getPayloadLengthAndMask(configurationBytesBuffer []byte, c net.Conn) (uint64, []byte, error) {
 	secondByte := uint(configurationBytesBuffer[1])
 	mask := make([]byte, 4)
@@ -110,4 +142,32 @@ func decodePayload(payload string, mask []byte) string {
 		message = append(message, payload[i]^mask[i%4])
 	}
 	return string(message)
+}
+
+// Sending messages functions
+func getPayloadLength(message string) []byte {
+	payloadLength := make([]byte, 0)
+	if len(message) < 126 {
+		payloadLength = append(payloadLength, byte(len(message)))
+	} else if len(message) < 65536 {
+		payloadLength = append(payloadLength, byte(126))
+		for i := 0; i < 2; i++ {
+			b := len(message) >> ((2 - i - 1) * 8) & 0xFF
+			payloadLength = append(payloadLength, byte(b))
+		}
+	} else {
+		payloadLength = append(payloadLength, byte(127))
+		for i := 0; i < 8; i++ {
+			b := len(message) >> ((2 - i - 1) * 8) & 0xFF
+			payloadLength = append(payloadLength, byte(b))
+		}
+	}
+	return payloadLength
+}
+
+func createFrame(message string, payloadLength []byte) []byte {
+	frame := make([]byte, 0, 1+len(payloadLength)+len(message))
+	frame = append(frame, 0b10000001)
+	frame = append(frame, payloadLength...)
+	return append(frame, message...)
 }
