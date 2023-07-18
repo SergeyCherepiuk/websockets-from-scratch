04667ff879fce867ca554680eca9afbea8553e48
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-07-18 07:35:59 +0200

    Hot fixed the package name

--- server/main.go
+++ server/main.go
@@ -5,7 +5,7 @@ import (
 	"net"
 	"strings"
 
-	"github.com/SergeyCherepiuk/websockets-test/websockets"
+	"github.com/SergeyCherepiuk/websockets-from-scratch/websockets"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
